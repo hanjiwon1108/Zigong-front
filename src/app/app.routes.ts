1be227b058413152255a8821c00279d75322bb04
hanjiwon1108 <jwjwhan1108@gmail.com>
2026-05-02 10:00:55 +0900

    refactor: route component 추가

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { Routes } from '@angular/router';
-import { Dashboard } from './pages/dashboard/dashboard';
-import { Simulator } from './pages/simulator/simulator';
+import { DashboardPage } from './pages/dashboard/dashboard';
+import { AnalyticsPage } from './pages/analytics/analytics';
+import { TransactionsPage } from './pages/transactions/transactions';
+import { SimulatorPage } from './pages/simulator/simulator';
 
 export const routes: Routes = [
-    { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
-    { path: 'dashboard', component: Dashboard },
-    { path: 'simulator', component: Simulator }
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: 'dashboard',    component: DashboardPage },
+  { path: 'analytics',   component: AnalyticsPage },
+  { path: 'transactions', component: TransactionsPage },
+  { path: 'simulator',   component: SimulatorPage },
 ];
